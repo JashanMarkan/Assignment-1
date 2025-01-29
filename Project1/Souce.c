700a79fa976b1be01b4617f82b8183d2e8989db3
JashanMarkan <jmarkan4589@conestogac.on.ca>
2025-01-29 13:27:29 -0500

    create a result variable for the addition

--- Project1/Souce.c
+++ Project1/Souce.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
 int main(void) {
-	const int Number1 = 10;
+	const int Number1 = 10 , Number2 = 43;
+	int result;
+	result = Number1 + Number2;
+	printf("\n The result = %d ", result);
+	return 0;
 }
